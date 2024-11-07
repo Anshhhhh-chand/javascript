141cf5bbdc93b839a64af4f7f623f7a27b3a984d
Anshhhhh-chand <145494166+Anshhhhh-chand@users.noreply.github.com>
2024-11-07 12:51:59 +0000

    container config done

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,4 @@
+"use strict"; //treat all js code as newer version
+// alert("hello") // we are using nodejs not browser
+console.log(1+2) ;
+console.log("ansh")
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,12 @@
+const accountid= 144553
+let accountemail="chandansh82@gmail.com"
+var accountpass="iansh"
+accountcity= "Lucknow"
+let acco;
+//accountid=2
+console.log(accountid);
+accountemail="chand4@gmail.com"
+var accountpass="iamansh"
+accountcity= "Luck"
+/*prefer not to use var because of issue in functional scope */
+console.table([accountid,accountemail,accountcity,accountpass,acco])
